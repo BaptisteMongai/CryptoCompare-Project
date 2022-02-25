23efb5d45953eedcfd4ca5eef314ddf52ecadbfb
theotimesv <theotime.soulier-verschaeve@efrei.net>
2022-02-25 15:27:20 +0100

    pre data auto-actualization

--- CryptoCompare-Project/DataHandling/DataScrapper.cs
+++ CryptoCompare-Project/DataHandling/DataScrapper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Newtonsoft.Json;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Controls;
 
@@ -45,55 +46,54 @@ namespace CryptoCompare_Project
     public class CryptoRatesDataScrapper
     {
         public List<double> crypto1ClosePrices { get; set; }
-        public List<int> crypto1Dates { get; set; }
+        public List<DateTime> crypto1Dates { get; set; }
         public List<double> crypto2ClosePrices { get; set; }
-        public List<int> crypto2Dates { get; set; }
-
+        public List<DateTime> crypto2Dates { get; set; }
+        
         public CryptoRatesDataScrapper()
         {
             crypto1ClosePrices = new List<double>();
-            crypto1Dates = new List<int>();
+            crypto1Dates = new List<DateTime>();
             crypto2ClosePrices = new List<double>();
-            crypto2Dates = new List<int>();
+            crypto2Dates = new List<DateTime>();
+            
         }
         
         public async Task ScrapDataCrypto1Function(string cryptoLink)
         {
             crypto1ClosePrices = new List<double>();
-            crypto1Dates = new List<int>();
+            crypto1Dates = new List<DateTime>();
             string key = "0edc1384280b50ac53679b94991868fb11fca894abeaf40290cbe2548199599f";
             
             using (var web2 = new HttpClient())
             {
                 web2.DefaultRequestHeaders.Add("Apikey", key);
-
                 var response = web2.GetAsync(cryptoLink).Result;
-
                 if (response.IsSuccessStatusCode)
                 {
                     var jsonString = response.Content.ReadAsStringAsync().Result;
+                    
                     var result = JsonConvert.DeserializeObject<CryptoHistDataRoot>(jsonString);
                     for (int i = 0; i < result.Data.Data.Count; i++)
                     {
                         crypto1ClosePrices.Add(result.Data.Data[i].close);
-                        crypto1Dates.Add(result.Data.Data[i].time);
+                        var dateTime = ConvertFromUnixTimestamp(result.Data.Data[i].time);
+                        crypto1Dates.Add(dateTime);
                     }
                 }
             }
         }
-        
-        public async Task  ScrapDataCrypto2Function(string cryptoLink)
+
+        public async Task ScrapDataCrypto2Function(string cryptoLink)
         {
             crypto2ClosePrices = new List<double>();
-            crypto2Dates = new List<int>();
+            crypto2Dates = new List<DateTime>();
             string key = "0edc1384280b50ac53679b94991868fb11fca894abeaf40290cbe2548199599f";
 
             using (var web3 = new HttpClient())
             {
                 web3.DefaultRequestHeaders.Add("Apikey", key);
-
                 var response = web3.GetAsync(cryptoLink).Result;
-
                 if (response.IsSuccessStatusCode)
                 {
                     var jsonString = response.Content.ReadAsStringAsync().Result;
@@ -101,10 +101,26 @@ namespace CryptoCompare_Project
                     for (int i = 0; i < result.Data.Data.Count; i++)
                     {
                         crypto2ClosePrices.Add(result.Data.Data[i].close);
-                        crypto2Dates.Add(result.Data.Data[i].time);
+                        var dateTime = ConvertFromUnixTimestamp(result.Data.Data[i].time);
+                        crypto2Dates.Add(dateTime);
                     }
                 }
             }
+        }
+        
+        static DateTime ConvertFromUnixTimestamp(int timestamp)
+        {
+            // Format our new DateTime object to start at the UNIX Epoch
+            System.DateTime dateTime = new System.DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+
+            // Add the timestamp (number of seconds since the Epoch) to be converted
+            dateTime = dateTime.AddSeconds(timestamp);
+		
+            //var dateTime2 = new DateTime(2015, 05, 24, 10, 2, 0, DateTimeKind.Local);
+            //var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            //var unixDateTime = (dateTime2.ToUniversalTime() - epoch).TotalSeconds;
+
+            return dateTime;
         }
     }
 }
