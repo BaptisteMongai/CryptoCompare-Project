f726eebf8d1ce132f9b68c29cb5708c637b8646b
Jean-Actimel <baptiste.mongai@gmail.com>
2022-02-28 10:16:49 +0100

    Notification.xaml.cs

---
+++ CryptoCompare-Project/Views/Notifications.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CryptoCompare_Project.Views
+{
+    public partial class Notifications : UserControl
+    {
+        public Notifications()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void Calcul_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
